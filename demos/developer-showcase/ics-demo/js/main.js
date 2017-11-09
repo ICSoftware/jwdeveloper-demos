76366f76016bc86fe32bea7584d006c25523fe10
Gleb Radutsky <gradutsky@industrialcolor.com>
2017-11-09 15:34:45 -0500

    Found version 8 player to use and moved js logic into html file.

--- demos/developer-showcase/ics-demo/js/main.js
+++ demos/developer-showcase/ics-demo/js/main.js
@@ -1,7 +1,10 @@
-var playerInstance = jwplayer("lbDiv");
-playerInstance.setup({
-	file: "//content.jwplatform.com/videos/aeZLVVPv-v7C1BfDE.mp4",
-	image: "//content.jwplatform.com/thumbs/aeZLVVPv-480.jpg",
-	height: "100%",
-	width: "70%"
-});
+// var playerInstance = jwplayer("lbDiv");
+// playerInstance.setup({
+// 	file: "//content.jwplatform.com/videos/aeZLVVPv-v7C1BfDE.mp4",
+// 	image: "//content.jwplatform.com/thumbs/aeZLVVPv-480.jpg",
+// 	// file: "//content.jwplatform.com/videos/xJ7Wcodt-xDYBr4fw.mp4",
+// 	// image: "//content.jwplatform.com/thumbs/xJ7Wcodt-480.jpg",
+// 	playbackRateControls: true
+// });
+
+var gleb = 1;
