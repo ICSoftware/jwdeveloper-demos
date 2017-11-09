ba8c5f6b0bef181d974b3dfda1e1b79f24387777
Gleb Radutsky <gradutsky@industrialcolor.com>
2017-11-09 12:45:35 -0500

    Copied player embed and script from branded-player-ads to since own video is having licensing issues.

--- demos/developer-showcase/ics-demo/js/main.js
+++ demos/developer-showcase/ics-demo/js/main.js
@@ -1,6 +1,7 @@
-jwplayer("player").setup({
-  "file": "/assets/Amazing_Caves_1080.mp4",
-  "height": 360,
-  "width": 640,
-  "autostart": "viewable"
+var playerInstance = jwplayer("lbDiv");
+playerInstance.setup({
+	file: "//content.jwplatform.com/videos/aeZLVVPv-v7C1BfDE.mp4",
+	image: "//content.jwplatform.com/thumbs/aeZLVVPv-480.jpg",
+	height: "100%",
+	width: "70%"
 });
